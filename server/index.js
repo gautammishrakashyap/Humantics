7ce503361071984153b69e4bb21095181c31cab2
gautammishrakashyap <kumargautam7071@gmail.com>
2025-03-27 19:23:55 +0530

    try

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express()
 
@@ -8,3 +9,8 @@ app.get("/", (req, res) => {
 app.listen(5000, () => {
     console.log("Server is running on Port 5000");
 });
+const ATLAS_URI = "mongodb+srv://jai251926:jai251926@cluster0.irgwh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+mongoose.connect(ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("Error connecting to MongoDB Atlas:", err));
